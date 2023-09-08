618f69c9efc76306adbd757114a093c938af54d8
wil-vse <wil@valuestreamengineers.com>
2023-09-08 11:59:46 -0500

    Adding repo to new code version

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,6 +1,7 @@
 module "solution" {
   source  = "aztfmod/caf/azurerm"
   version = "5.7.3"
+  source = "git::https://github.com/viking-cruises/terraform-azurerm-caf.git?ref=vrc-2023.09.08"
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
 
   providers = {
