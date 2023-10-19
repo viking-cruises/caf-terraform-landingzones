58fa9f06923c03b2c2febac3737c39ef85835679
wil-vse <wil@valuestreamengineers.com>
2023-10-19 18:46:26 -0500

    Update module version

--- caf_solution/landingzone.tf
+++ caf_solution/landingzone.tf
@@ -1,7 +1,7 @@
 module "solution" {
   source  = "aztfmod/caf/azurerm"
   version = "5.7.6"
-  # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
+  source = "git::https://github.com/viking-cruises/terraform-azurerm-caf.git?ref=vrc-2023.10.19"
 
   providers = {
     azurerm.vhub = azurerm.vhub
