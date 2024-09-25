dffd7e39242cb4fbf25f934923e24d157f322e0c
Patrick Moss <patrick.moss@viking.com>
2024-09-25 14:56:23 -0400

    version bump

--- caf_launchpad/landingzone.tf
+++ caf_launchpad/landingzone.tf
@@ -1,7 +1,7 @@
 module "launchpad" {
-  source = "git::https://github.com/viking-cruises/terraform-azurerm-caf.git?ref=vrc-2024.09.24"
-  #source  = "aztfmod/caf/azurerm"
-  #version = "5.7.13"
+  #source = "git::https://github.com/viking-cruises/terraform-azurerm-caf.git?ref=vrc-2024.09.24"
+  source  = "aztfmod/caf/azurerm"
+  version = "5.7.13"
   #source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
 
   providers = {
