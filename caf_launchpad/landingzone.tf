cf458e5b6d8bbc411a680f0cbd61977549727556
Patrick Moss <patrick.moss@viking.com>
2024-09-25 09:22:13 -0400

    Update landingzone.tf

--- caf_launchpad/landingzone.tf
+++ caf_launchpad/landingzone.tf
@@ -5,7 +5,7 @@ module "launchpad" {
   # source = "git::https://github.com/aztfmod/terraform-azurerm-caf.git?ref=main"
 
   providers = {
-    azurerm.vhub = azurerm.vhub
+    azurerm.vhub = "3.116.0"
   }
 
   current_landingzone_key               = var.landingzone.key
